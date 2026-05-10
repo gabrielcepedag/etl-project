b49ac93c8ad842296706d165cb111c1f1838ccdc
Gabriel Cepeda <gabrielcepeda8@gmail.com>
2026-05-10 12:25:08 -0400

    Feat: Creating data model for earthquakes

--- dbt/models/staging/earthquakes/STG_EARTHQUAKES.sql
+++ dbt/models/staging/earthquakes/STG_EARTHQUAKES.sql
@@ -1,3 +1,26 @@
-SELECT
-    *
-FROM {{ source("earthquakes", "raw_earthquakes")}}
+with source as (
+    select * from {{ source('earthquakes', 'raw_earthquakes') }}
+),
+cleaned as (
+    select
+        id,
+        mag,
+        place,
+        -- convertir epoch a timestamp
+        to_timestamp(time / 1000) as event_time,
+        to_timestamp(updated / 1000) as updated_time,
+        tsunami,
+        sig,
+        net,
+        code,
+        magtype,
+        type,
+        status,
+        -- PARSE coordinates
+        split_part(trim(both '[]' from coordinates), ',', 1)::float as longitude,
+        split_part(trim(both '[]' from coordinates), ',', 2)::float as latitude,
+        split_part(trim(both '[]' from coordinates), ',', 3)::float as depth,
+        inserted_at
+    from source
+)
+select * from cleaned
